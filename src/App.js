7829d8aaaf421b0d5b66edd97bbc1217208de8c5
dyvapandhu <dyvapandhu989@gmail.com>
2022-08-31 10:21:54 +0700

    [CHANGE] Router config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/Dashboard";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Kanban Demo</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<DashboardPage />} />
+          <Route path="/Login" exact element={<LoginPage />} />
+          <Route path="/Dashboard" exact element={<DashboardPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
